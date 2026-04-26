bef4162df0641b67458d39ac8ca7dc6e742326b8
Mày gay hả? <cykablyatt1505@gmail.com>
2026-04-26 19:41:23 +0700

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App';
-import * as serviceWorkerRegistration from './serviceWorkerRegistration'; // Thêm dòng này
 
 const container = document.getElementById('root');
 const root = createRoot(container);
@@ -12,5 +11,15 @@ root.render(
   </React.StrictMode>
 );
 
-// Đổi từ unregister() sang register() để kích hoạt PWA
-serviceWorkerRegistration.register();
+// ĐOẠN CODE ĐĂNG KÝ SERVICE WORKER ĐỂ BẬT TÍNH NĂNG PWA
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', () => {
+    navigator.serviceWorker.register('/sw.js')
+      .then(registration => {
+        console.log('ServiceWorker đăng ký thành công với scope: ', registration.scope);
+      })
+      .catch(err => {
+        console.log('ServiceWorker đăng ký thất bại: ', err);
+      });
+  });
+}
