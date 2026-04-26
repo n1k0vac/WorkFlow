0dcce848eef5e94cc09060ba2b59877ffe355600
Mày gay hả? <cykablyatt1505@gmail.com>
2026-04-26 19:36:22 +0700

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,11 +1,16 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
-import App from './App'; // Gọi file App.js bạn vừa tạo
+import App from './App';
+import * as serviceWorkerRegistration from './serviceWorkerRegistration'; // Thêm dòng này
 
 const container = document.getElementById('root');
 const root = createRoot(container);
+
 root.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
 );
+
+// Đổi từ unregister() sang register() để kích hoạt PWA
+serviceWorkerRegistration.register();
